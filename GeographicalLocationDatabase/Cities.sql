3dfe64baa671b9e1f1dae64a5ec13fa6001f1232
adrian-brace <adrian.brace@btinternet.com>
2018-11-05 13:23:28 +0000

    Added Database project to the solution and a DB model from database to the Service project

---
+++ GeographicalLocationDatabase/Cities.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Cities]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Name] NVARCHAR(128) NOT NULL, 
+    [SubRegion] NVARCHAR(128) NULL, 
+    [Country] NVARCHAR(64) NOT NULL, 
+    [TouristRating] TINYINT NULL, 
+    [EstablishedOn] DATE NULL, 
+    [EstimatedPopulation] INT NULL
+)
